b91491d8d1f85e9e25cf6d42aa74cad574de037a
kanalbekovich <kanalbekovich@gmail.com>
2025-07-04 02:02:48 +0000

    class2 py files

---
+++ Class2/while.py
@@ -0,0 +1,7 @@
+limit=10  # while it true it will keep executing
+count=0
+
+while count < limit:  
+    print(count)
+    count += 1
+
---
+++ Class2/year.py
@@ -0,0 +1,7 @@
+def days():
+    day = years*365
+    return day
+
+years=int(input("Enter number of years: "))
+x=days()
+print(x)
